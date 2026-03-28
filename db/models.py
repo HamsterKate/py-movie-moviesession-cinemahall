026b9792bb4e52c956e8eafd4dab7b8072517918
Kate <hamster.k.po@gmail.com>
2026-03-28 21:32:06 +0200

    f-string to format method in __str__ maethod + removed extra imports

--- db/models.py
+++ db/models.py
@@ -13,7 +13,7 @@ class Actor(models.Model):
     last_name = models.CharField(max_length=255)
 
     def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+        return "{} {}".format(self.first_name, self.last_name)
 
 
 class Movie(models.Model):
@@ -53,4 +53,4 @@ class MovieSession(models.Model):
     )
 
     def __str__(self) -> str:
-        return f"{self.movie.title} {self.show_time}"
+        return "{} {}".format(self.movie.title, self.show_time)
