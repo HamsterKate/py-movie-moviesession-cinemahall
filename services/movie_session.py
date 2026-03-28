bd714d1e64caf3b6a4323e7bcc495de716ce99c1
Kate <hamster.k.po@gmail.com>
2026-03-28 21:16:11 +0200

    added _id suffix in the keyword argument

--- services/movie_session.py
+++ services/movie_session.py
@@ -11,12 +11,12 @@ def create_movie_session(
 ) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
-        cinema_hall=cinema_hall_id,
-        movie=movie_id,
+        cinema_hall_id=cinema_hall_id,
+        movie_id=movie_id,
     )
 
 
-def get_movie_sessions(session_date: str = None) -> QuerySet[MovieSession]:
+def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
     if session_date:
         return MovieSession.objects.filter(show_time__date=session_date)
     else:
@@ -37,9 +37,9 @@ def update_movie_session(
     if show_time:
         session.show_time = show_time
     if movie_id:
-        session.movie = movie_id
+        session.movie_id = movie_id
     if cinema_hall_id:
-        session.cinema_hall = cinema_hall_id
+        session.cinema_hall_id = cinema_hall_id
     session.save()
     return session
 
