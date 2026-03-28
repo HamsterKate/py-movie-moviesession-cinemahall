a02ae2f1646a0e63459b8b9948bb45ae71e70794
Kate <hamster.k.po@gmail.com>
2026-03-28 21:07:45 +0200

    direct values assing + single  quotes replacement

--- services/movie_session.py
+++ services/movie_session.py
@@ -11,8 +11,8 @@ def create_movie_session(
 ) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
-        cinema_hall=CinemaHall.objects.get(id=cinema_hall_id),
-        movie=Movie.objects.get(id=movie_id),
+        cinema_hall=cinema_hall_id,
+        movie=movie_id,
     )
 
 
@@ -23,7 +23,7 @@ def get_movie_sessions(session_date: str = None) -> QuerySet[MovieSession]:
         return MovieSession.objects.all()
 
 
-def movie_session_by_id(movie_session_id: int) -> MovieSession:
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
 
@@ -31,19 +31,19 @@ def update_movie_session(
         session_id: int,
         show_time: datetime = None,
         movie_id: int = None,
-        cinemahall_id: int = None,
+        cinema_hall_id: int = None,
 ) -> MovieSession:
     session = MovieSession.objects.get(id=session_id)
     if show_time:
         session.show_time = show_time
     if movie_id:
-        session.movie = Movie.objects.get(id=movie_id)
-    if cinemahall_id:
-        session.cinema_hall = CinemaHall.objects.get(id=cinemahall_id)
+        session.movie = movie_id
+    if cinema_hall_id:
+        session.cinema_hall = cinema_hall_id
     session.save()
     return session
 
 
-def delete_movie_session(session_id: int) -> None:
+def delete_movie_session_by_id(session_id: int) -> None:
     session = MovieSession.objects.get(id=session_id)
     session.delete()
